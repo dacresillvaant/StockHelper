35a6b4077cf4afe07abca151bf444e8a57307ebc
dacresillvaant <mateusz.kubis94@gmail.com>
2025-03-13 21:02:50 +0100

    First prompt and succesfull connection

--- src/main/java/com/mateusz/springgpt/SpringGptApplication.java
+++ src/main/java/com/mateusz/springgpt/SpringGptApplication.java
@@ -1,13 +1,30 @@
 package com.mateusz.springgpt;
 
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringGptApplication {
+public class SpringGptApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringGptApplication.class, args);
-	}
+    @Autowired
+    OpenAiChatModel chatModel;
 
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGptApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+		Prompt prompt = new Prompt("Tell me if you have access to list of stocks listed on New York Stock Exchange from this month?");
+		System.out.println("Sending a prompt: " + prompt.getContents());
+
+		ChatResponse response = chatModel.call(prompt);
+		System.out.println("Full response is: " + response);
+		System.out.println("Prompt respose is: " + response.getResult().getOutput().getText());
+    }
 }
