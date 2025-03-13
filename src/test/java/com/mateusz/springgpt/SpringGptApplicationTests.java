7685c90b4a6917ebba6fedb42ab68608670f5564
dacresillvaant <mateusz.kubis94@gmail.com>
2025-03-13 20:46:31 +0100

    Initial commit

---
+++ src/test/java/com/mateusz/springgpt/SpringGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateusz.springgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
