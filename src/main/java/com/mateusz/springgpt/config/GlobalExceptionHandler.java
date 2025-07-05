0e304bffac1a9d881ccd7d58bbb157643fa1c987
dacresillvaant <mateusz.kubis94@gmail.com>
2025-07-05 11:56:30 +0200

    Refactor GlobalExceptionHandler

--- src/main/java/com/mateusz/springgpt/config/GlobalExceptionHandler.java
+++ src/main/java/com/mateusz/springgpt/config/GlobalExceptionHandler.java
@@ -24,6 +24,10 @@ public class GlobalExceptionHandler {
 
     private final MailgunEmailService mailgunEmailService;
 
+    private String getParam(HttpServletRequest request) {
+        return Optional.ofNullable(request.getQueryString()).orElse("");
+    }
+
     @Autowired
     public GlobalExceptionHandler(MailgunEmailService mailgunEmailService) {
         this.mailgunEmailService = mailgunEmailService;
@@ -31,43 +35,37 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResponseStatusException.class)
     public ResponseEntity<Map<String, Object>> handleResponseStatusException(ResponseStatusException exception, HttpServletRequest request) {
-        String param = Optional.ofNullable(request.getQueryString()).orElse("");
-
         Map<String, Object> responseBody = new LinkedHashMap<>();
         responseBody.put("timestamp", LocalDateTime.now());
         responseBody.put("status", exception.getStatusCode().value());
         responseBody.put("error", exception.getStatusCode());
         responseBody.put("message", exception.getReason());
-        responseBody.put("path", request.getRequestURI().concat(param));
+        responseBody.put("path", request.getRequestURI().concat(getParam(request)));
 
         return new ResponseEntity<>(responseBody, exception.getStatusCode());
     }
 
     @ExceptionHandler(NoResourceFoundException.class)
     public ResponseEntity<Map<String, Object>> handleNoResourceFoundException(NoResourceFoundException exception, HttpServletRequest request) {
-        String param = Optional.ofNullable(request.getQueryString()).orElse("");
-
         Map<String, Object> responseBody = new LinkedHashMap<>();
         responseBody.put("timestamp", LocalDateTime.now());
         responseBody.put("status", HttpStatus.NOT_FOUND.value());
         responseBody.put("error", HttpStatus.NOT_FOUND.getReasonPhrase());
         responseBody.put("message", "Static resource not found");
-        responseBody.put("path", request.getRequestURI().concat(param));
+        responseBody.put("path", request.getRequestURI().concat(getParam(request)));
 
-        log.debug("No static resource found {}", request.getRequestURI().concat(param));
+        log.debug("No static resource found {}", request.getRequestURI().concat(getParam(request)));
         return new ResponseEntity<>(responseBody, exception.getStatusCode());
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<Map<String, Object>> handleDataIntegrityViolationExceptions(Exception exception, HttpServletRequest request) {
-        String param = Optional.ofNullable(request.getQueryString()).orElse("");
-
         Map<String, Object> responseBody = new LinkedHashMap<>();
         responseBody.put("timestamp", LocalDateTime.now());
         responseBody.put("status", HttpStatus.CONFLICT.value());
         responseBody.put("error", HttpStatus.CONFLICT.getReasonPhrase());
         responseBody.put("message", exception.getMessage());
-        responseBody.put("path", request.getRequestURI().concat(param));
+        responseBody.put("path", request.getRequestURI().concat(getParam(request)));
 
         log.warn("Conflict while adding resource", exception);
         return new ResponseEntity<>(responseBody, HttpStatus.CONFLICT);
@@ -75,14 +73,12 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<Map<String, Object>> handleNoSuchElementExceptions(Exception exception, HttpServletRequest request) {
-        String param = Optional.ofNullable(request.getQueryString()).orElse("");
-
         Map<String, Object> responseBody = new LinkedHashMap<>();
         responseBody.put("timestamp", LocalDateTime.now());
         responseBody.put("status", HttpStatus.NOT_FOUND.value());
         responseBody.put("error", HttpStatus.NOT_FOUND.getReasonPhrase());
         responseBody.put("message", exception.getMessage());
-        responseBody.put("path", request.getRequestURI().concat(param));
+        responseBody.put("path", request.getRequestURI().concat(getParam(request)));
 
         log.warn("Data not found", exception);
         return new ResponseEntity<>(responseBody, HttpStatus.NOT_FOUND);
@@ -90,14 +86,12 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, Object>> handleAllExceptions(Exception exception, HttpServletRequest request) {
-        String param = Optional.ofNullable(request.getQueryString()).orElse("");
-
         Map<String, Object> responseBody = new LinkedHashMap<>();
         responseBody.put("timestamp", LocalDateTime.now());
         responseBody.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
         responseBody.put("error", HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
         responseBody.put("message", exception.getMessage());
-        responseBody.put("path", request.getRequestURI().concat(param));
+        responseBody.put("path", request.getRequestURI().concat(getParam(request)));
 
         log.error("Unhandled exception caught in global handler", exception);
         mailgunEmailService.sendErrorAlertEmail(exception, request);
